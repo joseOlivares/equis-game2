badea4eeac97e17cdc137d488e2a63caf9ac570f
José Luis <joseluis_503@yahoo.com>
2021-09-07 21:41:44 -0600

    modal set nickname

--- tic-tac-toe-olivares/src/app/home/home.component.ts
+++ tic-tac-toe-olivares/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+declare let UIkit: any;
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public nickName:any;
 
   constructor() { }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
+     /*
+    this.askNickName().then((nick)=>{
+      this.nickName=nick;
+      console.log(this.nickName);
+    });
+    */
+
+    UIkit.modal('#my-nickName').show();
+
   }
+
+  async askNickName():Promise<string>{
+     return UIkit.modal.prompt('Your nickname:','').then(function (nick: string) {
+      if (nick !== '' && nick !== null) {
+        return nick;
+      } else {
+        let randomNum=Math.floor(Math.random() * 100); //number between 0 and 1000;
+        return 'user'+randomNum;
+      }
+    });
+  }
+
 
 }
